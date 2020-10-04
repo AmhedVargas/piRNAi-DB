5649f77230d5223f9b4773c4a888f47d64e2d843
AmhedVargas <avargas0lcg@gmail.com>
2020-10-04 15:53:41 +0300

    Add scripts and data example

---
+++ C_briggsae-pipeline/C_brigsae_pipe.sh
@@ -0,0 +1,161 @@
+###Pipeline for C. briggase piRNA track based on multiple ones developed for C. elegans ###
+
+###Software ###
+#bedtools ##Not necesary but makes the things easier
+#bwa ##Or any other short aligner that produces sam files
+#samtools ##Necesary to work with sam/bam files
+#QueryInGenomeWithMMinWindow.pl #Custom made script
+###
+
+##Create working directory
+mkdir piRNADB_c_briggsae
+cd piRNADB_c_briggsae
+
+###Starting files####
+##Latest C. Briggsae CB4 / Essentially any version since WB254 will be the same##
+##Latest C. Briggsae (CB4) Worm base annotations##  ##WS274 at the moment
+wget ftp://ftp.wormbase.org/pub/wormbase/releases/WS274/species/c_briggsae/PRJNA10731/c_briggsae.PRJNA10731.WS274.annotations.gff3.gz
+wget ftp://ftp.wormbase.org/pub/wormbase/releases/WS274/species/c_briggsae/PRJNA10731/c_briggsae.PRJNA10731.WS274.genomic.fa.gz
+
+#Uncompress genome
+gzip -d c_briggsae.PRJNA10731.WS274.genomic.fa.gz
+#Index it
+bwa index c_briggsae.PRJNA10731.WS274.genomic.fa
+###
+
+###Command lines###
+##Obtain coordinates of all the Wormbase CDS and convert them into BED format. Please note that gff are 1-based format and bed are 0-based##
+zcat c_briggsae.PRJNA10731.WS274.annotations.gff3.gz | awk -F"\t" '{if($2=="WormBase"){if($3=="CDS"){print $0}}}' - | awk -F"\t" '{OFS="\t"; if($7 == "-"){str="+"}else{str="-"}; print $1,($4-1),$5,$9,$6,str}' > Cbriggsae-CDS_WB274.bed
+
+###Obtain fasta sequences of CDS in 5` to 3` orientation (strand dependent) ###
+bedtools getfasta -s -fi c_briggsae.PRJNA10731.WS274.genomic.fa -bed Cbriggsae-CDS_WB274.bed -name+ > Cbriggsae-CDS_WB274.fasta
+
+
+### Unique sites only
+##Convert each CDS into 20-mers 
+#0 MM
+awk '{if($0 ~ />/){name=$0;}else{for(i=1; i<=(length($0)-19); i++){print name"_"i"separatortoto"substr($0,i,20)"\n"substr($0,i,20)}}}' Cbriggsae-CDS_WB274.fasta > CDS-20mers.fasta
+
+##Look for uniqueness in 20 mers with perl script (hash method)
+../QueryInGenomeWithMMinWindow2.pl c_briggsae.PRJNA10731.WS274.genomic.fa CDS-20mers.fasta 20 0 1 20 20.tab
+
+##Convert into fasta files sequences that appear only once, place it as 20mer and remove Ts.
+awk -F"\t" '{if($3 == 1){print $1}}' 20.tab | awk -F"separatortoto" '{print $1"\n"$2}' | awk '{if($0 ~ />/){name=$0}else{seq=substr($0,1,3); if(!(seq ~ /T/)){print name"\n"$0}}}' - > Unique-20mers-noT.fasta
+
+##Obtain positions of uniquely mapped by bwa
+bwa aln -n 0 -o 0 c_briggsae.PRJNA10731.WS274.genomic.fa Unique-20mers-noT.fasta | bwa samse c_briggsae.PRJNA10731.WS274.genomic.fa - Unique-20mers-noT.fasta > Cbri_gen_0MM_piRNA-seqs.sam
+grep "XT:A:U" Cbri_gen_0MM_piRNA-seqs.sam | awk -F"\t" '{OFS="\t";if($2==16){str="-"}else{str="+"}print $3,($4-1),($4+19),$1,$6,str}' | sort -k1,1 -k2,2n > Cbri_gen_0MM_piRNA-seqs.bed
+
+###1 mismatch by perl strategy only
+##Convert into 18-mers then add 1 MM
+#1 MM
+awk '{if($0 ~ />/){name=$0;}else{for(i=1; i<=(length($0)-19); i++){print name"_"i"separatortoto"substr($0,i,20)"\n"substr($0,i+1,18)}}}' Unique-20mers-noT.fasta > CDS-18mers.fasta
+
+##Look for uniqueness in 18 mers with perl script (hash method)
+../QueryInGenomeWithMMinWindow2.pl c_briggsae.PRJNA10731.WS274.genomic.fa CDS-18mers.fasta 18 0 1 18 18.tab
+
+##Convert into fasta files sequences that appear only once, place it as 20mer; Ts have been removed before.
+awk -F"\t" '{if($3 == 1){print $1}}' 18.tab | awk -F"separatortoto" '{print $1"\n"$2}' - > Unique-18mers-noT.fasta
+
+##Filter for up to one MM as a 20mer
+../QueryInGenomeWithMMinWindow.pl c_briggsae.PRJNA10731.WS274.genomic.fa Unique-18mers-noT.fasta 20 1 2 19 | awk -F"\t" '{if($3==1){print $1"\n"$2}}' > Unique18mers-1MM-noT.fasta
+
+##Obtain positions of uniquely mapped by bwa
+bwa aln -n 0 -o 0 c_briggsae.PRJNA10731.WS274.genomic.fa Unique18mers-1MM-noT.fasta | bwa samse c_briggsae.PRJNA10731.WS274.genomic.fa - Unique18mers-1MM-noT.fasta > Cbri_gen_1MM_piRNA-seqs.sam
+grep "XT:A:U" Cbri_gen_1MM_piRNA-seqs.sam | awk -F"\t" '{OFS="\t";if($2==16){str="-"}else{str="+"}print $3,($4-1),($4+19),$1,$6,str}' | sort -k1,1 -k2,2n > Cbri_gen_1MM_piRNA-seqs.bed
+
+###2 mismatches by blast strategy only
+##Create DB and filter for alignements larger than 17
+#2 MM
+
+##After installing blast, it is required to create a database of the genome to map against
+makeblastdb -in c_briggsae.PRJNA10731.WS274.genomic.fa -dbtype 'nucl' -title c_briggsae.WS274
+
+##Blast filtered unique 18 mer sequences using "blast short algorithm"
+blastn -db c_briggsae.PRJNA10731.WS274.genomic.fa -query Unique18mers-1MM-noT.fasta -task "blastn-short" -out Blast18mers-1MM.tab -outfmt 6 -evalue 1 -num_threads 20
+
+##Get names of seqs that are unique in a set that matches at least 18 bp and start the aligment before the third position
+awk -F"\t" '{if((($3*$4)/100)>17){if($7 < 3){array[$1]++}}} END{for (key in array){if(array[key]==1){print key}}}' Blast18mers-1MM.tab > Blast_18mers_filt-2MM.txt
+
+##Parse output to create a file with the indexes of the unique sequences
+awk '{if($0 ~ />/){name=$0;}else{print name"\t"$0}}' Unique18mers-1MM-noT.fasta | awk -F">" '{print $2}' | awk -F"\t" '{if(array[$1] != 0){print ">"$1"\n"array[$1]}else{array[$1]=$2}}' - Blast_18mers_filt-2MM.txt > Cbri_gen_2MM_piRNA-seqs.fasta
+
+##Map unique to obtain position
+bwa aln -n 0 -o 0 c_briggsae.PRJNA10731.WS274.genomic.fa Cbri_gen_2MM_piRNA-seqs.fasta | bwa samse c_briggsae.PRJNA10731.WS274.genomic.fa - Cbri_gen_2MM_piRNA-seqs.fasta > Cbri_gen_2MM_piRNA-seqs.sam
+grep "XT:A:U" Cbri_gen_2MM_piRNA-seqs.sam| awk -F"\t" '{OFS="\t";if($2==16){str="-"}else{str="+"}print $3,($4-1),($4+19),$1,$6,str}' | sort -k1,1 -k2,2n > Cbri_gen_2MM_piRNA-seqs.bed
+
+###3 mismatches by blast strategy only
+## Use previous alignement and filter from it
+#3 MM
+
+##Get names of seqs that are unique in a set that matches at least 18 bp and start the aligment before the third position
+awk -F"\t" '{if((($3*$4)/100)>16){if($7 < 4){array[$1]++}}} END{for (key in array){if(array[key]==1){print key}}}' Blast18mers-1MM.tab > Blast_18mers_filt-3MM.txt
+
+##Parse output to create a file with the indexes of the unique sequences
+awk '{if($0 ~ />/){name=$0;}else{print name"\t"$0}}' Unique18mers-1MM-noT.fasta | awk -F">" '{print $2}' | awk -F"\t" '{if(array[$1] != 0){print ">"$1"\n"array[$1]}else{array[$1]=$2}}' - Blast_18mers_filt-3MM.txt > Cbri_gen_3MM_piRNA-seqs.fasta
+
+##Map unique to obtain position
+bwa aln -n 0 -o 0 c_briggsae.PRJNA10731.WS274.genomic.fa Cbri_gen_3MM_piRNA-seqs.fasta | bwa samse c_briggsae.PRJNA10731.WS274.genomic.fa - Cbri_gen_3MM_piRNA-seqs.fasta > Cbri_gen_3MM_piRNA-seqs.sam
+grep "XT:A:U" Cbri_gen_3MM_piRNA-seqs.sam | awk -F"\t" '{OFS="\t";if($2==16){str="-"}else{str="+"}print $3,($4-1),($4+19),$1,$6,str}' | sort -k1,1 -k2,2n > Cbri_gen_3MM_piRNA-seqs.bed
+
+###4 mismatches by 16-mer and blast strategy
+## Use previous unique mer fasta and convert it into 16 mer (2MM-16-2MM)
+#4 MM
+awk '{if($0 ~ />/){name=$0;}else{for(i=1; i<=(length($0)-19); i++){print name"_"i"separatortoto"substr($0,i,20)"\n"substr($0,i+2,16)}}}' Cbri_gen_3MM_piRNA-seqs.fasta > 3MM-CDS-16mers.fasta
+
+##Look for uniqueness in 18 mers with perl script (hash method)
+../QueryInGenomeWithMMinWindow2.pl c_briggsae.PRJNA10731.WS274.genomic.fa 3MM-CDS-16mers.fasta 16 0 1 16 16.tab
+
+##Convert into fasta files sequences that appear only once, place it as 20mer; Ts have been removed before.
+awk -F"\t" '{if($3 == 1){print $1}}' 16.tab | awk -F"separatortoto" '{print $1"\n"$2}' - > Unique-16mers-noT-3MM.fasta
+
+##Blast filtered unique 16 mer sequences using "blast short algorithm"
+blastn -db c_briggsae.PRJNA10731.WS274.genomic.fa -query Unique-16mers-noT-3MM.fasta -task "blastn-short" -out Blast16mers-3MM.tab -outfmt 6 -evalue 1 -num_threads 20
+
+##Get names of seqs that are unique in a set that matches at least 16 bp and start the aligment before the third position
+awk -F"\t" '{if((($3*$4)/100)>15){if($7 < 5){array[$1]++}}} END{for (key in array){if(array[key]==1){print key}}}' Blast16mers-3MM.tab > Blast_16mers_filt-4MM.txt
+
+##Parse output to create a file with the indexes of the unique sequences
+awk '{if($0 ~ />/){name=$0;}else{print name"\t"$0}}' Unique-16mers-noT-3MM.fasta | awk -F">" '{print $2}' | awk -F"\t" '{if(array[$1] != 0){print ">"$1"\n"array[$1]}else{array[$1]=$2}}' - Blast_16mers_filt-4MM.txt > Cbri_gen_4MM_piRNA-seqs.fasta
+
+##Map unique to obtain position
+bwa aln -n 0 -o 0 c_briggsae.PRJNA10731.WS274.genomic.fa Cbri_gen_4MM_piRNA-seqs.fasta | bwa samse c_briggsae.PRJNA10731.WS274.genomic.fa - Cbri_gen_4MM_piRNA-seqs.fasta > Cbri_gen_4MM_piRNA-seqs.sam
+grep "XT:A:U" Cbri_gen_4MM_piRNA-seqs.sam| awk -F"\t" '{OFS="\t";if($2==16){str="-"}else{str="+"}print $3,($4-1),($4+19),$1,$6,str}' | sort -k1,1 -k2,2n > Cbri_gen_4MM_piRNA-seqs.bed
+
+###4 mismatches by 15-mer sided to right and then a final blast
+## Use previous unique mer fasta and convert it into 15 mer (5MM-15 unique)
+#5 MM
+awk '{if($0 ~ />/){name=$0;}else{for(i=1; i<=(length($0)-19); i++){print name"_"i"separatortoto"substr($0,i,20)"\n"substr($0,i+5,15)}}}' Cbri_gen_4MM_piRNA-seqs.fasta > 4MM-CDS-R15mers.fasta
+
+##Look for uniqueness in 15 mers with perl script (hash method)
+../QueryInGenomeWithMMinWindow2.pl c_briggsae.PRJNA10731.WS274.genomic.fa 4MM-CDS-R15mers.fasta 15 0 1 15 15.tab
+
+##Convert into fasta files sequences that appear only once, place it as 20mer; Ts have been removed before.
+awk -F"\t" '{if($3 == 1){print $1}}' 15.tab | awk -F"separatortoto" '{print $1"\n"$2}' - > Unique-R15mers-noT-4MM.fasta
+
+##Blast filtered unique 16 mer sequences using "blast short algorithm"
+blastn -db c_briggsae.PRJNA10731.WS274.genomic.fa -query Unique-R15mers-noT-4MM.fasta -task "blastn-short" -out Blast15mers-4MM.tab -outfmt 6 -evalue 1 -num_threads 20
+
+##Get names of seqs that are unique in a set that matches at least 16 bp and start the aligment before the third position
+awk -F"\t" '{if((($3*$4)/100)>14){if($7 < 6){array[$1]++}}} END{for (key in array){if(array[key]==1){print key}}}' Blast15mers-4MM.tab > Blast_16mers_filt-5MM.txt
+
+##Parse output to create a file with the indexes of the unique sequences
+awk '{if($0 ~ />/){name=$0;}else{print name"\t"$0}}' Unique-R15mers-noT-4MM.fasta | awk -F">" '{print $2}' | awk -F"\t" '{if(array[$1] != 0){print ">"$1"\n"array[$1]}else{array[$1]=$2}}' - Blast_16mers_filt-5MM.txt > Cbri_gen_5MM_piRNA-seqs.fasta
+
+#Align sequences back to the genome and convert map to a bed file
+bwa aln -n 0 -o 0 c_briggsae.PRJNA10731.WS274.genomic.fa Cbri_gen_5MM_piRNA-seqs.fasta | bwa samse c_briggsae.PRJNA10731.WS274.genomic.fa - Cbri_gen_5MM_piRNA-seqs.fasta > Cbri_gen_5MM_piRNA-seqs.sam
+grep "XT:A:U" Cbri_gen_5MM_piRNA-seqs.sam | awk -F"\t" '{OFS="\t";if($2==16){str="-"}else{str="+"}print $3,($4-1),($4+19),$1,$6,str}' | sort -k1,1 -k2,2n > Cbri_gen_5MM_piRNA-seqs.bed
+
+
+
+###Please note that any of the fasta files were filtered for GC content; in case that that GC filtering is needed, it can be done in the fasta files with the following command:
+#: awk '{if($0 ~ />/){name=$0;}else{sumA=0;sumT=0;sumC=0;sumG=0;sumN=0;seq=$0;k=length(seq); for (i=1;i<=k;i++) {if (substr(seq,i,1)=="T") sumT+=1; else if (substr(seq,i,1)=="A") sumA+=1; else if (substr(seq,i,1)=="G") sumG+=1; else if (substr(seq,i,1)=="C") sumC+=1; else if (substr(seq,i,1)=="N") sumN+=1}; GCcontent=(sumC+sumG)/k*100; if((GCcontent >= 30) && (GCcontent <= 45)){print name"_GC-"GCcontent"\n"seq}}}' File.fasta > GCFilt.fasta
+## Where 30 is lower boundary and 45 is upper boundary, and File.fasta is input fasta and GCfilt.fasta is name of output fasta.
+##Also, this file can be mapped once again with bwa and converted in bed with following command:
+#: bwa aln -n 0 -o 0 c_briggsae.PRJNA10731.WS274.genomic.fa GCFilt.fasta | bwa samse c_briggsae.PRJNA10731.WS274.genomic.fa - GCFilt.fasta > GCFilt.sam
+
+#: grep "XT:A:U" GCFilt.sam | awk -F"\t" '{OFS="\t";if($2==16){str="-"}else{str="+"}print $3,($4-1),($4+19),$1,$6,str}' | sort -k1,1 -k2,2n > GCFilt.bed
+
+
+
+
---
+++ C_elegans-pipeline/QueryInGenomeWithMMinWindow2.pl
@@ -0,0 +1,176 @@
+#!/usr/bin/perl
+
+#####Documentation######
+$num_args = $#ARGV + 1;
+if ($num_args != 7) {
+  print "\nUsage: QueryInGenomeWithMMinWindow.pl Ref_fasta Query_fasta k-mer_length mm_in_window Pos-start_window Pos-end_window Out_file\n\n";
+  print "\tRef_fasta: Fasta file of genomic sequence to map. It can be multi-fasta\n";
+  print "\tQuery_seq: Set of k-mers that are going to be mapped\n";
+  print "\tk-mer_length: Size of subsequences that the reference file will be partitioned into. The k-mer length has to be the same size as the length of each query seq\n";
+  print "\tmm_in_window: Number of missmatches allowed in within a defined region of the query\n";
+  print "\tPos-start_window: Position that the window starts. The first bp is considered to be in the position 1\n";
+  print "\tPos-end_window: Position that the window ends.\n";
+  print "\tFile to output number of matches.\n";
+
+  print "\n\nNote: This program relies on storing data in RAM and can be too greedy for long k-mers (e.g. 4**20 subsequences can be stored for a k-mer of length 20)\n";
+  exit;
+}
+############
+
+##Reading and verifying input commands
+my $Ref=$ARGV[0];
+my $Query=$ARGV[1];
+my $k=$ARGV[2];
+my $mm=$ARGV[3];
+my $pwin1=$ARGV[4];
+my $pwin2=$ARGV[5];
+my $outF=$ARGV[6];
+
+###If the following files are not present -> stop the program
+if (!(-e $Ref."")){die $Ref." file not found!\n";}
+if (!(-e $Query."")){die $Name." file not found!\n";}
+if ($pwin2 > $k ){die "Window size cannot be larger than the k-mer length\n";}
+if ($mm > $k ){die "Total number or missmatches cannot be larger than the k-mer length\n";}
+if ($pwin1 < 1 ){die "The window start position has to be positive\n";}
+if ($pwin2 < 1 ){die "The window end position has to be positive\n";}
+if ($pwin2 < $pwin1 ){die "The window end position has to be larger than the start position\n";}
+
+$pwin1 = $pwin1 - 1;
+$pwin2 = $pwin2 - 1; 
+#@dict=("A","T","C","G");
+
+##Reading and hashing genomic sequence
+my $string="";
+open(op,$Ref) or die "cannot open Ref file\n";
+while($line=<op>){
+chomp($line);
+if(($line =~ m/^>/)){$string=$string."__________"; next;}
+$string=$string.$line
+}
+close(op);
+
+$Genomic = $string;
+$Revcomp = reverse $Genomic;
+$Revcomp =~ tr/ACGTacgt/TGCAtgca/;
+
+#Hash both strands
+#print "\nIndexing genome\n";
+my %hash;
+for($i=0;$i<=(length($Genomic)-$k);$i++){
+$hash{substr($Genomic,$i,$k)}++;
+$hash{substr($Revcomp,$i,$k)}++;
+}
+
+##Read each query and perform actions
+my $Qseq;
+my @array;
+my $sarray;
+my $val;
+open(op,$Query) or die "cannot open query file\n";
+open(oout,">".$outF) or die "cannot write output file\n";
+#print "\nReading sequences\n";
+##my $con=0;
+while($line=<op>){
+##$con++;
+chomp($line);
+if($line =~ m/^>/){$Qseq=$line}else{
+if(length($line) != $k){die $Qseq." query sequence has a different size (".length($Qseq).") than k-mer-length (".$k.").\n";}
+#if($hash{$line}>1){next;}
+#$val=$hash{$line};
+#my $flag=1;
+#print $Qseq."\t".$line."\t".$val."\n"}}
+
+##If mm larger than 0 go for array approach otherwise print hashed value
+$val = 0;
+if($mm == 0){
+  $val=$hash{$line};
+  }else{
+    #print "Line to test".$line."\n";
+    @array = produceSeqs($line,$pwin1,$pwin2,$mm);
+    foreach $finito(@array){
+      #print "\nHola:".$finito."\n";
+      $val = $val + $hash{$finito}; 
+      if(length($finito) != $k){die "Amhed you did something weird with the sequence ".$line.' producing '.$finito}}
+  }
+
+print oout $Qseq."\t".$line."\t".$val."\n";
+#if(flag ==1){print $Qseq."\t".$line."\t".$val."\n";}
+} #Stop if not header
+##if(!($con % 5)){print "\n".$con." sequences analyzed\n";}
+} ##Stop reading file
+close(op);
+close(oout);
+exit;
+
+###Sub function: Produce $line $start $end $number of mismatches
+sub produceSeqs {
+  #print "\nA\n";
+  @seq=split('',$_[0]);
+  #print @seq;
+  #print "\n";
+  $left="";
+  $right="";
+  if($_[1] > 0){$left= $left.join('',@seq[0..($_[1]-1)])}
+  if(($_[2]+1) < scalar(@seq)){$right= $right.join('',@seq[($_[2]+1)..(scalar(@seq)-1)])}
+  #print "Left:".$left."\n";
+  #print "Right:".$right."\n";
+  @temp = repseq($_[3],@seq[$_[1]..$_[2]]);
+  @seqs=();
+  foreach $tem (@temp){push(@seqs,$left.$tem.$right)}
+  #print "\nSeqsA\n";
+  #print @seqs;
+  return(@seqs);
+}
+
+sub repseq{
+  #print "\nB Args\n";
+  @args=@_;
+  #print @args;
+  $rep=$args[0];
+  #print "\nRepetitions:".$rep;
+  @letters=@args[1..(scalar(@args)-1)];
+  #print "\n Args again\n";
+  #print @args;
+  #print "\nSize:".scalar(@args);
+  #print "\nMaxIndex:".$#args;
+  #print "\nletters:\n";
+  #print @letters;
+  #@dict=("A","T","C","G");
+  @res=();
+  push(@res,join('',@letters));
+  my %rest;
+  #$size = keys %rest;
+  #print "\nAvantResB\n";
+  #print $size;
+  while($rep != 0){
+    @res=perseq(@res);
+    $rep--;
+  }
+  foreach $s (@res){$rest{$s}++}
+  #print "\nResB\n";
+  #$size = keys %rest;
+  #print $size;
+  return(keys(%rest));
+}
+
+sub perseq{
+  #print "\nC\n";
+  @fin=();
+  foreach $sequence (@_){
+    #print "\nSequence inside C:\n";
+    #print "-".$sequence."-";
+    @pat=split('',$sequence);
+    for($j=0; $j< scalar(@pat);$j++){
+      @tmep=@pat;
+      $tmep[$j]="A";
+      push(@fin,join("",@tmep));
+      $tmep[$j]="T";
+      push(@fin,join("",@tmep));
+      $tmep[$j]="C";
+      push(@fin,join("",@tmep));
+      $tmep[$j]="G";
+      push(@fin,join("",@tmep));
+    }
+  }
+  return(@fin);
+}
